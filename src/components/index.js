fe5e2a432e557174e05771b4ffab511bea92e043
HectorGerardoZV <torrozapata@gmail.com>
2022-11-20 22:35:12 -0700

    Menu component was created

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
 import Input from "./input/Input";
 import TextArea from "./textarea/TextArea";
-
-export { Input, TextArea };
+import Menu from "./menu/Menu";
+export { Input, TextArea, Menu };
--- src/pages/admin/adminMasterPage/AdminMasterPage.jsx
+++ src/pages/admin/adminMasterPage/AdminMasterPage.jsx
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router-dom";
-
+//Components
+import {Menu} from "../../../components";
 //Style
 import style from "./AdminMasterPage.module.css";
 const AdminMasterPage = () => {
     return (
-        <section>
-            <h1>MainPage</h1>
+        <section className={style.masterLayout}>
+            <Menu/>
             <Outlet />
         </section>
     );
